c3b87c91ef8b2ddee416da5d82fa720c9ec788cb
Blagoja Evkoski <Blagoja.Evkoski@netcetera.com>
2017-03-17 22:01:07 +0100

    Implement triggering of a native android toast
    This commit will implement triggering of a native android toast from the JavaScript part of the application.

---
+++ android/app/src/main/java/com/reactnativeandroidweardemo/ToastModule.java
@@ -0,0 +1,41 @@
+package com.reactnativeandroidweardemo;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
+
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "NativeToastAndroid";
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constants;
+    }
+
+    @ReactMethod
+    public void show(String message, int duration) {
+        Toast.makeText(getReactApplicationContext(), message, duration).show();
+    }
+
+}
--- index.android.js
+++ index.android.js
@@ -1,31 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
+  Button,
   View
 } from 'react-native';
+import NativeToastAndroid from './NativeToastAndroid';
 
 export default class reactNativeAndroidWearDemo extends Component {
+  onPress() {
+    NativeToastAndroid.show('This is a native toast!', NativeToastAndroid.SHORT);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Button
+          onPress={this.onPress}
+          title="Display Native Toast"
+        />
       </View>
     );
   }
@@ -37,17 +32,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('reactNativeAndroidWearDemo', () => reactNativeAndroidWearDemo);

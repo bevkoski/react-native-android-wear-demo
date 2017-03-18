b2419d130e07afe3e5a6671f4816dd2ff040b9da
Blagoja Evkoski <Blagoja.Evkoski@netcetera.com>
2017-03-18 11:43:02 +0100

    Remove toasts and obsolete imports
    This commit will remove the toast triggers and obsolete imports in the wear communication module.

--- android/app/src/main/java/com/reactnativeandroidweardemo/WearCommunicationModule.java
+++ android/app/src/main/java/com/reactnativeandroidweardemo/WearCommunicationModule.java
@@ -1,25 +1,20 @@
 package com.reactnativeandroidweardemo;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.widget.Toast;
 
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-import com.facebook.react.uimanager.IllegalViewOperationException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
 
-import java.nio.charset.Charset;
 import java.util.List;
 
 public class WearCommunicationModule extends ReactContextBaseJavaModule
@@ -53,7 +48,6 @@ public class WearCommunicationModule extends ReactContextBaseJavaModule
 
   @ReactMethod
   public void increaseWatchCounter() {
-    Toast.makeText(getReactApplicationContext(), "Increase watch counter!", Toast.LENGTH_SHORT).show();
     final List<Node> nodes = Wearable.NodeApi.getConnectedNodes(googleApiClient).await().getNodes();
     for (Node node : nodes) {
       Wearable.MessageApi.sendMessage(googleApiClient, node.getId(), "/increase_wear_counter", null);
@@ -63,7 +57,6 @@ public class WearCommunicationModule extends ReactContextBaseJavaModule
   @Override
   public void onMessageReceived(MessageEvent messageEvent) {
     if (messageEvent.getPath().equals("/increase_phone_counter")) {
-      Toast.makeText(getReactApplicationContext(), "Increase phone counter!", Toast.LENGTH_SHORT).show();
       sendEvent(getReactApplicationContext(), "increaseCounter", null);
     }
   }
